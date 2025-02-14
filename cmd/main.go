2886525908fa1575e273cb6cd15cd6f134e1b04d
Denis <denis@MacBook-Pro-Denis.local>
2025-02-14 23:09:02 +0300

    правки после ревью

--- cmd/main.go
+++ cmd/main.go
@@ -7,22 +7,22 @@ import (
 	"net"
 
 	"github.com/brianvoe/gofakeit"
-	"github.com/microservices-golang/auth/pkg/user"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"google.golang.org/protobuf/types/known/emptypb"
 	"google.golang.org/protobuf/types/known/timestamppb"
+
+	"github.com/microservices-golang/auth/pkg/user"
 )
 
 const grpcPort = 50051
-
-//protoc  --go_out=./pkg/user  --go-grpc_out=./pkg/user api/user/auth.proto
 
 type server struct {
 	user.UnimplementedUserServiceServer
 }
 
-func (s *server) Get(_ context.Context, req *user.GetUserRequest) (*user.GetUserResponse, error) {
+// GetUser - получить данные о пользователе
+func (s *server) GetUser(_ context.Context, req *user.GetUserRequest) (*user.GetUserResponse, error) {
 	id := req.GetId()
 	return &user.GetUserResponse{
 		Id:        id,
@@ -34,18 +34,21 @@ func (s *server) Get(_ context.Context, req *user.GetUserRequest) (*user.GetUser
 	}, nil
 }
 
-func (s *server) Create(_ context.Context, _ *user.CreateUserRequest) (*user.CreateUserResponse, error) {
+// CreateUser - создает нового пользователя
+func (s *server) CreateUser(_ context.Context, _ *user.CreateUserRequest) (*user.CreateUserResponse, error) {
 	return &user.CreateUserResponse{
 		Id: 2,
 	}, nil
 }
 
-func (s *server) Delete(_ context.Context, req *user.DeleteUserRequest) (*emptypb.Empty, error) {
+// DeleteUser - удаление пользователя
+func (s *server) DeleteUser(_ context.Context, req *user.DeleteUserRequest) (*emptypb.Empty, error) {
 	fmt.Println(req.GetId())
 	return &emptypb.Empty{}, nil
 }
 
-func (s *server) Update(_ context.Context, req *user.UpdateUserRequest) (*emptypb.Empty, error) {
+// UpdateUser - обновление данных пользователя
+func (s *server) UpdateUser(_ context.Context, req *user.UpdateUserRequest) (*emptypb.Empty, error) {
 	fmt.Println(req.GetId())
 	return &emptypb.Empty{}, nil
 }
